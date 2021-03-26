18b00ae36355505f12534f5ad654730803f82475
林子佑 <qqq23939889@gmail.com>
2021-03-26 22:33:23 +0800

    fix: 修復heroku http 判定問題

--- app.js
+++ app.js
@@ -63,10 +63,10 @@ app.use(express.static(__dirname+'/public'));
 //---------- CLIENT 頁面 ----------//
 app.get('/client', function(req, res){
     res.render('index-client', {
-        protocol: (!req.headers.host.includes('heroku'))? req.protocol: "http",
+        protocol: req.protocol,
         domain: req.headers.host,
         hostname: req.hostname,
-        secure: (req.protocol=="https" || !req.headers.host.includes('heroku'))? true: false,
+        secure: (req.protocol=="https")? true: false,
         joinID: req.query.joinID || "",
         customID: req.query.customID || "",
         needPort: !req.headers.host.includes('heroku')
